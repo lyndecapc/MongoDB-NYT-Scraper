d7144af5c18938ccf5e994f714dbb9b484237c5a
Lynde Capogreco <lyndecapc@gmail.com>
2020-04-22 14:42:02 -0500

    starting scrape

---
+++ scripts/scrape.js
@@ -0,0 +1,11 @@
+//scrape scripts
+//require request and cheerio to make scrapes possible
+
+var request = require("request");
+var cheerio = require("cheerio");
+
+var scrape = function (cb) {
+    
+}
+
+module.exports = scrape;
