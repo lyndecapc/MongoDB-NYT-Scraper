47b98fb1b1ec53a9424c31d5a5ef72c45a71f0ed
Lynde Capogreco <lyndecapc@gmail.com>
2020-04-21 19:02:57 -0500

    mongoose connection successful

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 //Require Dependencies
 var express = require("express");
+var mongoose = require("mongoose");
+var expressHandlebars = require("express-handlebars");
+var bodyParser = require("body-parser");
 
 //setup port & instantiate our express app
 var PORT = process.env.PORT || 3000;
@@ -9,9 +12,32 @@ var app = express();
 var router = express.Router();
 
 //designates our public folder as a static directory
-app.use(express.static(_dirname + "/public"));
+//app.use(express.static(_dirname + "/public"));
+
+//connect handlebars to our Express app
+app.engine("handlebars", expressHandlebars({
+    defaultLayout: "main"
+}));
+app.set("view engine", "handlebars");
+
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
 
 app.use(router);
+
+//if deployed, use the deployed database, otherwise use the local mongoHeadlines db
+var db = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+
+//connect mongoose to our database
+mongoose.connect(db, function(error) {
+    if(error) {
+        console.log(error);
+    }
+    else {
+        console.log("mongoose connection is successful");
+    }
+});
 
 //listen on the port
 app.listen(PORT, function() {
