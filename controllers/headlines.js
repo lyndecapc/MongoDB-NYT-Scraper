09fcb7a49bcfb02af95d91a2877686249401a87f
Lynde Capogreco <lyndecapc@gmail.com>
2020-04-23 20:50:03 -0500

    headlines and note mongoose models

---
+++ controllers/headlines.js
@@ -0,0 +1,23 @@
+//Bring in our scrape script and makeDate script
+var scrape = require("../scripts/scrape");
+var makeDate = require("../scripts/date");
+
+//Bring in the Headline and Note mongoose models
+var Headline = require("../models/Headline");
+
+module.exports = {
+    fetch: function (cb) {
+        scrape(function (data) {
+            var articles = data;
+            for (var i = 0; i < articles.length; i++) {
+                articles[i].date = makeDate();
+                articles[i].saved = false;
+            }
+            Headline.collection.insertMany(articles, {
+                ordered: false
+            }, function (err, docs) {
+                cb(err, docs);
+            });
+        });
+    }
+}
