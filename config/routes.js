04bb70ba752fbbb13105f7d65a6c5e379cfb47b3
Lynde Capogreco <lyndecapc@gmail.com>
2020-04-23 22:04:02 -0500

    get delete update routes

--- config/routes.js
+++ config/routes.js
@@ -29,5 +29,27 @@ module.exports = function (router) {
             }
         });
 
+        router.get("/api/headlines", function (req, res) {
+            var query = {};
+            if (req.query.saved) {
+                query = req.query;
+            }
+
+            headlinesController.get(query, function (data) {
+                res.json(data);
+            });
+        });
+        router.delete("/api/headlines/:id", function (req, res) {
+            var query = {};
+            query._id = req.params.id;
+            headlinesController.delete(query, function (err, data) {
+                res.json(data);
+            });
+        });
+        router.patch("/api/headlines", function(req, res){
+            headlinesController.update(req.body, function(err, data){
+                res.json(data);
+            });
+        });
     })
 }
